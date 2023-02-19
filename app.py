b0ea518ba2f4f1fb80e0a8792a793d2d7b920cce
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-02-19 15:17:37 -0500

    Fix unusual occurrences

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# merge conflict
-
 import pandas, os
 from flask import Flask, render_template
 
@@ -34,18 +32,7 @@ headings = ('Dated Published', 'Crime Category', 'City', 'Zip Code', 'Address', 
 
 graph_crime_type = []
 graph_crime_type_list = []
-<<<<<<< HEAD
 simplified_graph = []
-=======
-simplified_graph_crime_type = []
-# robbery = []
-# residential_burglary = []
-# commercial_burglary = []
-# vehicle_theft = []
-# vehicle_break_in = []
-# home_invasion_robbery = []
-# weapon_violation = []
->>>>>>> origin/master
 
 for i in data_array:
     graph_crime_type.append(i[1])
@@ -54,24 +41,13 @@ graph_crime_type = set(graph_crime_type)
 for i in graph_crime_type:
     graph_crime_type_list.append(i)
 graph_crime_type_list.sort()
-<<<<<<< HEAD
-=======
-print(graph_crime_type_list)
->>>>>>> origin/master
 
 for i in range(len(graph_crime_type)):
     crime_count = 0
     for j in range(len(data_array)):
         if graph_crime_type_list[i] == data_array[j][1]:
             crime_count += 1
-<<<<<<< HEAD
     simplified_graph.append((graph_crime_type_list[i], crime_count))
-=======
-    simplified_graph_crime_type.append((graph_crime_type_list[i], crime_count))
-
-for i in simplified_graph_crime_type:
-    print(i)
->>>>>>> origin/master
 
 
 @app.route('/')
@@ -85,7 +61,6 @@ def chart():
 
 
 @app.route('/graph/')
-<<<<<<< HEAD
 def graph():
     labels = [row[0] for row in simplified_graph]
     values = [row[1] for row in simplified_graph]
@@ -95,12 +70,6 @@ def graph():
 @app.route('/map/')
 def maps():
     return render_template('map.html')
-=======
-def graphs():
-    labels = [row[0] for row in simplified_graph_crime_type]
-    values = [row[1] for row in simplified_graph_crime_type]
-    return render_template('graph.html', labels=labels, values=values)
->>>>>>> origin/master
 
 
 @app.route('/about/')
